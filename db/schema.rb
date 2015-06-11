1d110d8b8fd5a877b0e3738fe7d28ae7f8b018ad
Carly Ho <carlymho@gmail.com>
2015-06-11 15:35:04 -0500

    Start working on character sheet page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611183621) do
+ActiveRecord::Schema.define(version: 20150611201351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20150611183621) do
     t.datetime "updated_at",         null: false
     t.integer  "character_type_id"
     t.integer  "chronicle_id"
+    t.string   "concept"
   end
 
   create_table "chronicle_allows_character_types", force: :cascade do |t|
