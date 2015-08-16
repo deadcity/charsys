9d8648861ec92093c295f59a9cb06eff37634283
cmho <carlymho@gmail.com>
2015-08-15 22:52:55 -0500

    Throw error when registration information is bad; also, validate user fields and uniqueness of email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ApplicationController
 	def create
 		user = params[:user]
 		@user = User.new({username: user[:username], name: user[:name], email: user[:email], password: params[:password]})
-		if @user.save!
+		if @user.valid?
+			@user.save!
 			flash[:success] = "Thanks for registering! You're now logged in."
 			session[:user_id] = @user.id
 		else
-			flash[:error] = "There was an error creating your account."
+			flash[:alert] = "There was an error creating your account."
 		end
-		redirect_to :root
+		redirect_to register_path
 	end
 
 	def edit
