15b54b69400dd359d5eb1eb177c5b8eb087dfd8f
cmho <carlymho@gmail.com>
2015-08-17 20:34:43 -0500

    fix order of associations

--- app/models/attrib.rb
+++ app/models/attrib.rb
@@ -1,4 +1,4 @@
 class Attrib < ActiveRecord::Base
-	has_many :characters, through: :character_has_attribute
 	has_many :character_has_attributes
+	has_many :characters, through: :character_has_attribute
 end
