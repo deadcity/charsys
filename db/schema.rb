279d5fa673ea4d5bea130d71838b486d8334fe56
Carly Ho <carlymho@gmail.com>
2015-08-18 22:45:50 -0500

    Add status column to characters and also put a thing in the character_controller to interpret the status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818185428) do
+ActiveRecord::Schema.define(version: 20150819034312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,8 +132,8 @@ ActiveRecord::Schema.define(version: 20150818185428) do
     t.integer  "health"
     t.integer  "willpower"
     t.integer  "max_resource"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.integer  "character_type_id"
     t.integer  "chronicle_id"
     t.string   "misc"
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 20150818185428) do
     t.integer  "armor_general"
     t.text     "st_notes"
     t.text     "printed_notes"
+    t.integer  "status",             default: 0
   end
 
   create_table "chronicle_allows_character_types", force: :cascade do |t|
