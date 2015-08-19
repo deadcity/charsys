751226ac03cfcc9d83931ce102a4ef4824863282
Stephanie Marx <stephanie.n.marx@gmail.com>
2015-08-18 21:52:55 -0500

    Change root to characters/index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'characters#index'
 
   resources :characters
   resources :chronicles
