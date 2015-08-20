965a41a5bf712843d62ce261015787d06b082c3c
Carly Ho <carlymho@gmail.com>
2015-08-20 10:47:56 -0500

    Add character status column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820025454) do
+ActiveRecord::Schema.define(version: 20150820154639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -181,6 +181,7 @@ ActiveRecord::Schema.define(version: 20150820025454) do
     t.integer  "socialize",          default: 1
     t.integer  "streetwise",         default: 1
     t.integer  "subterfuge",         default: 1
+    t.integer  "status",             default: 0
   end
 
   create_table "chronicle_allows_character_types", force: :cascade do |t|
