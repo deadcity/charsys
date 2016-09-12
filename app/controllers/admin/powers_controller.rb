ef18509c68119a1d10c7ec51caf9a912132e7a67
cmho <carlymho@gmail.com>
2016-09-11 23:10:32 -0500

    Start adding db backing for more robust power scaffolding; add forms for power creation and editing

--- app/controllers/admin/powers_controller.rb
+++ app/controllers/admin/powers_controller.rb
@@ -1,37 +1,46 @@
 module Admin
 	class PowersController < ApplicationController
 		def index
-
+			@powers = PowerType.all
 		end
 
 		def show
-
+			@power = Power.find(params[:id])
 		end
 
 		def new
-
+			@power = Power.new
 		end
 
 		def create
-
+			@power = Power.new(powers_params)
+			
 		end
 
 		def edit
-
+			@power = Power.find(params[:id])
 		end
 
 		def update
-
+			@power = Power.find(params[:id])
+			if @power.update_attributes!(powers_params)
+				flash[:success] = "#{@power.name} successfully updated."
+				redirect_to admin_powers_path
+			else
+				flash[:error] = "There was an error updating #{@power.name}."
+				redirect_to edit_admin_power_path(@power)
+			end
 		end
 
 		def destroy
-
+			@power = Power.find(params[:id])
+			@power.delete
 		end
 
 		private
 
 		def powers_params
-			
+			params.require(:power).permit(:name, :power_group_id)
 		end
 	end
 end
---
+++ db/migrate/20160912035355_power_group_has_power.rb
@@ -0,0 +1,4 @@
+class PowerGroupHasPower < ActiveRecord::Migration
+  def change
+  end
+end
